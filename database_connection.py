bf9240e3eaf62dd69c8f5cdc70671b60f96572d0
Görkem Çetin <91285958+gorkemcetinn@users.noreply.github.com>
2025-03-08 13:25:52 +0300

    Update database_connection.py

--- database_connection.py
+++ database_connection.py
@@ -3,7 +3,7 @@ import pandas as pd
 from llama_index.core.tools.tool_spec.base import BaseToolSpec
 
 class GaleriTools(BaseToolSpec):
-    """Galeri Sistemi Veritabanı Bağlantıları ve Sorguları"""
+    """Galeri Sistemi Veritabanı Bağlantıları ve Sorguları Çalıştırır"""
 
     spec_functions = ["get_car", "add_car", "update_car_price", "delete_car"]
 
@@ -13,7 +13,7 @@ class GaleriTools(BaseToolSpec):
         self.connection = pyodbc.connect(self.conn_str)
 
     def get_car(self) -> pd.DataFrame:
-        """Tüm arabalaro getirir."""
+        """Tüm araçları getirir."""
 
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM arabalar")
@@ -21,7 +21,7 @@ class GaleriTools(BaseToolSpec):
         return rows  # Pandas DataFrame yerine direkt liste döndürüyoruz.
 
     def add_car(self, marka: str, model: str, yil: int, fiyat: int, renk: str,plaka: str):
-        """Yeni çalışan ekler."""
+        """Yeni araç ekler."""
 
         query = "INSERT INTO arabalar (marka, model, yil, fiyat, renk, plaka) VALUES (?, ?, ?, ?, ?, ?)"
         cursor = self.connection.cursor()
@@ -30,7 +30,7 @@ class GaleriTools(BaseToolSpec):
         return f"{marka} {model} {yil} başarıyla eklendi."
 
     def update_car_price(self, marka: str, model: str, yeni_fiyat: int):
-        """Çalışanın maaşını günceller."""
+        """Araç bilgilerini günceller."""
         query = "UPDATE arabalar SET fiyat = ? WHERE marka = ? AND model = ?"
         cursor = self.connection.cursor()
         cursor.execute(query, (yeni_fiyat, marka, model))
@@ -38,7 +38,7 @@ class GaleriTools(BaseToolSpec):
         return f"{marka} {model} aracı fiyatı {yeni_fiyat} olarak güncellendi."
 
     def delete_car(self, marka: str, model: str):
-        """Çalışanı siler."""
+        """Araç siler."""
         query = "DELETE FROM arabalar WHERE marka = ? AND model = ?"
         cursor = self.connection.cursor()
         cursor.execute(query, (marka,model,))
